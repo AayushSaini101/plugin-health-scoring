9350c67bbdefbdef2381fe37c2be635162bc233d
AayushSaini101 <kumaraayush9810@gmail.com>
2024-02-21 21:48:12 +0530

    Remove checkstyle violation

--- core/src/test/java/io/jenkins/pluginhealth/scoring/probes/DefaultBranchBuildStatusProbeTest.java
+++ core/src/test/java/io/jenkins/pluginhealth/scoring/probes/DefaultBranchBuildStatusProbeTest.java
@@ -1,49 +1,12 @@
 package io.jenkins.pluginhealth.scoring.probes;
 
-
-
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.spy;
-
-import java.io.IOException;
-
-import org.junit.jupiter.api.Test;
-import org.kohsuke.github.GHCommit;
-
-
 
 class DefaultBranchBuildStatusProbeTest extends  AbstractProbeTest<DefaultBranchBuildStatusProbe> {
 
     @Override
     DefaultBranchBuildStatusProbe getSpy() {
         return spy(DefaultBranchBuildStatusProbe.class);
-    }
-
-    @Test
-    void shouldNotRequireRelease() {
-        assertThat(getSpy().requiresRelease()).isFalse();
-    }
-
-    @Test
-    void shouldBeRelatedToCode() {
-        assertThat(getSpy().isSourceCodeRelated()).isTrue();
-    }
-
-    @Test
-    void shouldVerifyDefaultBranch() throws IOException {
-
-//        final io.jenkins.pluginhealth.scoring.model.Plugin plugin = mock(Plugin.class);
-//        final ProbeContext ctx = mock(ProbeContext.class);
-//        when(plugin.getName()).thenReturn("foo");
-//        when(ctx.getScmRepository()).thenReturn(Optional.empty());
-//        GHRepository ghRepository = new GHRepository();
-//        ghRepository.setDefaultBranch("main");
-            GHCommit ghCommit = new GHCommit();
-
-
-//
-
-
     }
 
 }
